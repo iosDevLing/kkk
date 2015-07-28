95a906dd1ded8e60a214d8b0a91082cbdff8de5c
凌壮山 <lingzhuangshan@MyiMac.local>
2015-07-28 10:42:28 +0800

    完成功能2

---
+++ kkk/component/gongneng2.h
@@ -0,0 +1,13 @@
+//
+//  gongneng2.h
+//  kkk
+//
+//  Created by 凌壮山 on 15/7/28.
+//  Copyright (c) 2015年 凌壮山. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface gongneng2 : UIView
+
+@end
---
+++ kkk/component/gongneng2.m
@@ -0,0 +1,21 @@
+//
+//  gongneng2.m
+//  kkk
+//
+//  Created by 凌壮山 on 15/7/28.
+//  Copyright (c) 2015年 凌壮山. All rights reserved.
+//
+
+#import "gongneng2.h"
+
+@implementation gongneng2
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
